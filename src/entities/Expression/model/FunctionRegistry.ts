5fce831fff3177fa1f3bc5bc4d000b177f96b6df
Egor Kondratev <kondratevegor04@gmail.com>
2024-07-11 23:28:24 +0300

    feat: add more mathematical functions

--- src/entities/Expression/model/FunctionRegistry.ts
+++ src/entities/Expression/model/FunctionRegistry.ts
@@ -2,5 +2,19 @@ export type MathFunc = (a: number) => number;
 export type FunctionRegistry = Record<string, MathFunc>;
 
 export const functionRegistry: FunctionRegistry = Object.freeze({
-  abs: Math.abs
+  abs: Math.abs,
+  acos: Math.acos,
+  acosh: Math.acosh,
+  asin: Math.asin,
+  asinh: Math.asinh,
+  atan: Math.atan,
+  atanh: Math.atanh,
+  cos: Math.cos,
+  cosh: Math.cosh,
+  log10: Math.log10,
+  sin: Math.sin,
+  sinh: Math.sinh,
+  sqrt: Math.sqrt,
+  tan: Math.tan,
+  tanh: Math.tanh,
 });
